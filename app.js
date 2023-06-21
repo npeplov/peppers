be1d15a2bac8946d4f6f78d9184adaa746b09a64
Nikita Peplov <npeplov@yandex.ru>
2023-06-21 15:30:00 +0300

    [+] timer class, results at end of game

--- app.js
+++ app.js
@@ -9,16 +9,18 @@ const fieldWrapper = document.querySelector(".field__wrapper");
 // - какой градус
 // 7 страница старта
 // 8 страница правил
-// 9 таймер 3-2-1 старт
-// v 10 при лвлапе вывести 
+// v 10 при лвлапе вывести
 // v модалка position: absolute,
 // v zindex 2
 // v transform: translate()
 // v 11 при неправильном X и новый экран
+// v 13 вывести итоги если таймер закончен
+// v поместить в модалку
+// v overflow hidden
 
-// 13 вывести итоги если таймер закончен
+// 9 таймер 3-2-1 
+// - старт игры
 
-// 12 history of results
 // 8d3dca
 // 3 подсчет очков
 // - нейронов синим - добавляются в прогрессбар
@@ -27,15 +29,22 @@ const fieldWrapper = document.querySelector(".field__wrapper");
 // - без харкод величин в контейнере
 // - тустринг ===
 
+const delay = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
 const results = {
   current: 105,
   attempts: 4,
   right: 2,
   ip: 100,
   neurons: 3,
-}
+};
 
 const showSummary = () => {
+  const summaryModal = document.querySelector(".summary");
+  summaryModal.classList.add("slidein");
+
   const resultCurrentSpan = document.querySelector("#result__current");
   const resultRightSpan = document.querySelector("#result__right");
   const resultAccuracySpan = document.querySelector("#result__accuracy");
@@ -44,28 +53,36 @@ const showSummary = () => {
 
   resultCurrentSpan.innerText = results.current;
   resultRightSpan.innerText = `${results.right} из ${results.attempts}`;
-  resultAccuracySpan.innerText = `${(results.right/results.attempts*100).toFixed(0)}%`;
+  resultAccuracySpan.innerText = `${(
+    (results.right / results.attempts) *
+    100
+  ).toFixed(0)}%`;
   resultIpSpan.innerText = results.ip;
   resultNeuronsSpan.innerText = results.neurons;
 };
 
-const timer = () => {
-  const timerSpan = document.querySelector("#timer");
+class TimerClass {
+  constructor(seconds) {
+    this.seconds = seconds;
+  }
 
-  let seconds = 60;
-  let timerId = setInterval(() => {
-    timerSpan.innerText = `00:${seconds.toString().padStart(2, "0")}`;
-    if (seconds > 0) seconds -= 1;
-    else {
-      showSummary();
-      clearInterval(timerId);
-    }
-  }, 1000);
-};
+  timer() {
+    const timerSpan = document.querySelector("#timer");
+    let timerId = setInterval(() => {
+      timerSpan.innerText = `00:${this.seconds.toString().padStart(2, "0")}`;
+      if (this.seconds > 0) {
+        this.seconds -= 1;
+      } else {
+        clearInterval(timerId);
+      }
+    }, 1000);
+  }
+}
+
+const timerObj = new TimerClass(2);
 
 const createField = (level = 1) => {
-  
-  const appWrapper = document.querySelector('.app__wrapper');
+  const appWrapper = document.querySelector(".app__wrapper");
   const field = document.createElement("div");
   field.classList.add("grid-container");
   fieldWrapper.appendChild(field);
@@ -91,7 +108,7 @@ const fillField = (level = 1) => {
   const { numbers, digits, animation } = levels[level];
 
   const numbersArr = getRndNumbersArr(digits, numbers);
-  const targetInd = getRndNumber(numbers-1, 0);
+  const targetInd = getRndNumber(numbers - 1, 0);
   targetNum = numbersArr[targetInd];
 
   goal.innerText = targetNum;
@@ -100,14 +117,14 @@ const fillField = (level = 1) => {
     const button = document.createElement("button");
     const span = document.createElement("span");
     button.style.background = getRndColor();
-    button.classList.add("game__button")
+    button.classList.add("game__button");
     button.appendChild(span);
 
     if (i === targetInd) span.innerText = targetNum;
     else span.innerText = numbersArr[i];
 
     field.appendChild(button);
-    
+
     span.style.animationName = animation;
   }
 
@@ -131,15 +148,22 @@ function checkNumber({ target }) {
     levelSpan.innerText = `${level}-9`;
 
     correctImg.style.display = "block";
-  }
-  else {
+  } else {
     notcorrectImg.style.display = "block";
   }
 
-  answerStatusModal.style.animationName = "slideIn";
+  answerStatusModal.style.animationName = "slideInOut";
   removeField();
   createField(level);
   fillField(level);
+  checkGameOver();
+}
+
+const checkGameOver = async () =>{
+  if (timerObj.seconds === 0) {
+    await delay(1000)
+    showSummary();
+  }
 }
 
 const getRndColor = (max = 5) => {
@@ -188,8 +212,13 @@ const levels = {
 let targetNum;
 let level = 1;
 
-window.onload = async () => {
+const startGame = () => {
+  level = 1;
   createField(level);
   fillField(level);
-  timer();
+  timerObj.timer();
+}
+
+window.onload = async () => {
+  startGame();
 };
